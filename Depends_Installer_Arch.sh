67b87ffe645d57948a3743a9e8eb8234ae33208f
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 09:39:29 -0700

    Update Depends_Installer_Arch.sh

--- Depends_Installer_Arch.sh
+++ Depends_Installer_Arch.sh
@@ -1,4 +1,4 @@
 sudo pacman install nmcli nmap wireshark tcpdump aircrack-ng kismet -y
 #add fix for complete even if one fails to install
 #Test to varify on arch
-#Add error loging
+#Add error loging or force display with no quit on end
