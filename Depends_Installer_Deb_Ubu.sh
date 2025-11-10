80c1dafe94e18aacc4ec7bd9709200b337318e6f
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 09:36:53 -0700

    Update and rename Depends_Installer_Deb_Ubu.sh to Depends_Installer_Ubuntu_Plucky_25.04.sh

--- Depends_Installer_Deb_Ubu.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install nmcli nmap wireshark tcpdump aircrack-ng -y
-#add fix for complete even if one fails to install
-#install kismet todo
-#Add error loging
---
+++ Depends_Installer_Ubuntu_Plucky_25.04.sh
@@ -0,0 +1,7 @@
+sudo apt install nmcli nmap wireshark tcpdump aircrack-ng -y
+#add fix for complete even if one fails to install
+#Add error loging
+wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/plucky plucky main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
+sudo apt update
+sudo apt install kismet
