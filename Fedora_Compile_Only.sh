016333d3128646ef654ec789d5f3c643d24a6fd3
Mole <157280874+MoleSof@users.noreply.github.com>
2025-11-10 13:47:49 -0700

    Create Fedora_Compile_Only.sh

---
+++ Fedora_Compile_Only.sh
@@ -0,0 +1,4 @@
+sudo dnf install gcc gcc-c++ zlib-devel sqlite3 sqlite-devel openssl-devel \
+libwebsockets libwebsockets-devel libpcap libpcap-devel libusb1 libusb1-devel \
+rtl-sdr rtl-sdr-devel mosquitto mosquitto-devel lm_sensors lm_sensors-devel \
+libnl3-devel NetworkManager-libnm-devel
